4fb4e4f1208188b969db3cc5873a665cba3e060c
WildTrack <wildryx@outlook.fr>
2022-12-08 20:39:00 +0100

    feat(api): :sparkles: Update controller

--- api/src/controllers/gift.controller.js
+++ api/src/controllers/gift.controller.js
@@ -9,3 +9,63 @@ const giftOptions = (request) => {
   }
 }
 
+// /////////////////////
+// Add new gift controller
+exports.addGift = (req, res) => {
+    const {hoodId, userId} = req.params;
+
+    const gift = new GiftModel(giftOptions(req.body));
+    gift
+        .save()
+        .then((gift) => {
+            HoodModel.findByIdAndUpdate
+            (hoodId, {$push: {gifts: {userId: userId, gift: gift._id}}}, {new: true})
+                .then((hood) => {
+                    res.status(204).json(hood);
+                })
+        })
+        .catch(() => {
+            res.status(500).json("An error occurred!");
+        });
+};
+
+// /////////////////////
+// Edit gift controller
+exports.editGift = (req, res) => {
+    const {hoodId, giftId} = req.params;
+
+    const gift = new GiftModel(giftOptions(req.body));
+
+    GiftModel
+        .findById
+        (giftId, gift, {new: true})
+        .then((gift) => {
+            HoodModel
+                .findByIdAndUpdate
+                (hoodId, {$push: {gifts: {gift: gift._id}}}, {new: true})
+                .then((hood) => {
+                    res.status(204).json(hood);
+                })
+        })
+        .catch(() => {
+            res.status(500).json("An error occurred!");
+        });
+};
+
+// /////////////////////
+// Delete gift controller
+exports.deleteGift = (req, res) => {
+    const {hoodId, giftId} = req.params;
+
+    GiftModel.findByIdAndDelete(giftId).then((gift) => {
+        HoodModel.findByIdAndUpdate
+        (hoodId, {$pull: {gifts: {gift: gift._id}}}, {new: true})
+            .then((hood) => {
+                res.status(204).json(hood);
+            })
+    })
+        .catch(() => {
+            res.status(500).json("An error occurred!");
+        });
+};
+
--- api/src/controllers/hood.controller.js
+++ api/src/controllers/hood.controller.js
@@ -1,5 +1,4 @@
 const HoodModel = require("../models/hood.model");
-const path = require("path");
 
 const hoodOptions = (request) => {
   return {
@@ -11,33 +10,30 @@ const hoodOptions = (request) => {
   }
 }
 
-// //////////////////
-// Get all controller
-exports.getAll = (_req, res) => {
-  HoodModel.find().then((hoods) => {
-    res.json(hoods);
+// /////////////////////
+// Get all hoods by userId controller
+exports.getAllHoodsByUserId = (req, res) => {
+  const userId = req.params.id;
+
+  HoodModel.find({ "gifts.userId": userId }).then((hoods) => {
+    res.status.json(hoods);
+  });
+};
+
+// /////////////////////
+// Get hood by id controller
+exports.getHoodById = (req, res) => {
+  const hoodId = req.params.id;
+
+  HoodModel.findById(hoodId).populate("gifts.userId").populate("gifts.gift").then((hood) => {
+    res.json(hood);
   });
 };
 
 // //////////////////
-// Get one controller
-exports.getOne = (req, res) => {
-  const id = req.params.id;
-
-  HoodModel.findById(id).then((hood) => {
-      res.json(hood);
-    })
-    .catch((error) => {
-      res.status(500).json({
-        message: error,
-      });
-    });
-};
-
-// //////////////////
-// Add controller
-exports.add = (req, res) => {
-  const hood = new HoodModel(HoodOptions(req.body));
+// Create hood controller
+exports.createHood = (req, res) => {
+  const hood = new HoodModel(hoodOptions(req.body));
   hood
     .save()
     .then((_message) => {
@@ -49,16 +45,13 @@ exports.add = (req, res) => {
 };
 
 // //////////////////
-// Update controller
-exports.update = (req, res) => {
-  const findById = { _id: req.params.id };
+// Update hood controller
+exports.updateHood = (req, res) => {
+  const hoodId = req.params.id;
+  const hood = new HoodModel(hoodOptions(req.body));
 
-  HoodModel.findOneAndReplace(findById, hoodOptions(req.body), { new: true })
-    .then((_message) => {
-      res.json("Your hood has been updated!");
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
-
+  HoodModel.findByIdAndUpdate(hoodId, hood, { new: true }).then((_message) => {
+    res.json("Your hood has been updated!");
+  });
 };
+
