8589d5057083e5a816da122f0451e92a91155fc8
GERARD Alexandre <agerard57@protonmail.com>
2022-12-08 22:19:59 +0100

    feat: Add navigation

--- client/src/navigation/components/Navigation.tsx
+++ client/src/navigation/components/Navigation.tsx
@@ -1,24 +1,38 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomePage } from "../../homePage";
+import { HoodsPage } from "../../hoodsPage";
+import { RootTabParamList } from "../../types";
+import { Layout } from "../../layout";
 
 export const Navigation = () => {
-  type RootTabParamList = {
-    Home: undefined;
-    "todo-add": undefined;
-    //"todo-edit": { id: string };
+  const { Navigator, Screen } = createNativeStackNavigator<RootTabParamList>();
+
+  const NavContainerTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: "transparent",
+    },
   };
 
-  const { Navigator, Screen } = createNativeStackNavigator<RootTabParamList>();
   return (
-    <NavigationContainer>
-      <Navigator>
-        <Screen
-          name="Home"
-          component={HomePage}
-          //options={{ title: "Welcome" }}
-        />
-      </Navigator>
+    <NavigationContainer theme={NavContainerTheme}>
+      <Layout>
+        <Navigator initialRouteName="Home">
+          <Screen
+            name="Home"
+            component={HomePage}
+            //options={{ title: "Welcome" }}
+            options={{ headerShown: false }}
+          />
+          <Screen
+            name="Hoods"
+            component={HoodsPage}
+            options={{ headerShown: false }}
+          />
+        </Navigator>
+      </Layout>
     </NavigationContainer>
   );
 };
