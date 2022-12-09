e39e610688e0a7410d6f2a150dd99aaa7cfea5dc
WildTrack <wildryx@outlook.fr>
2022-12-09 18:39:31 +0100

    refactor: :recycle: Refactor style

--- client/src/core/components/Input.tsx
+++ client/src/core/components/Input.tsx
@@ -1,54 +1,69 @@
-import { useLinkProps } from "@react-navigation/native";
-import { Component, Dispatch, FC, ReactNode, SetStateAction, useContext, useState } from "react";
-import { StyleProp, View, ViewStyle, StyleSheet, Text, TextInput, TextInputProps } from "react-native";
+import { FC, useContext } from "react";
+import {
+  StyleProp,
+  View,
+  ViewStyle,
+  Text,
+  TextInput,
+  TextInputProps,
+  StyleSheet,
+} from "react-native";
+
 import { ThemeContext, ThemeType } from "../../theme";
 
 type Props = {
   label: String;
   additionalStyling?: StyleProp<ViewStyle>;
   textInputParams?: TextInputProps;
-  inputValue: String,
-  updateInputValue: Dispatch<SetStateAction<String>>
+  updateInputValue: (text: string) => void;
 };
 
-
-export const Input: FC<Props> = ({ label, additionalStyling, textInputParams, inputValue, updateInputValue }) => {
+export const Input: FC<Props> = ({
+  label,
+  additionalStyling,
+  textInputParams,
+  updateInputValue,
+}) => {
   const theme = useContext(ThemeContext);
 
   return (
-    <View style={[ additionalStyling, {
-      width: "100%",
-      marginVertical: "0.75rem",
-    } ]}>
-      <Text
-        style={{
-          fontSize: 20,
-          fontFamily: theme && theme.fonts.semiBold,
-          color: theme.colors.black,
-          textAlign: "left",
-          marginLeft: "0.6rem",
-        }}>
-        {label}
-      </Text>
-      <TextInput 
-      onChangeText={(input) => updateInputValue(input)}
+    <View style={[additionalStyling, styles().view]}>
+      <Text style={styles().label}>{label}</Text>
+      <TextInput
+        onChangeText={updateInputValue}
         {...textInputParams}
-        style={{
-          backgroundColor: theme.colors.white,
-          width: "100%",
-          height: "2.5em",
-          marginTop: "0.5rem",
-          borderRadius: 10,
-          paddingHorizontal: 15,
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 0,
-            height: 1,
-          },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-        }}
-        />
+        style={styles().input}
+      />
     </View>
   );
 };
+
+const styles = (theme?: ThemeType) =>
+  StyleSheet.create({
+    view: {
+      width: "100%",
+      marginVertical: "0.75rem",
+    },
+    label: {
+      fontSize: 20,
+      fontFamily: theme && theme.fonts.semiBold,
+      color: theme && theme.colors.black,
+      textAlign: "left",
+      marginLeft: "0.6rem",
+    },
+    input: {
+      backgroundColor: theme && theme.colors.white,
+      width: "100%",
+      height: "2.5em",
+      marginTop: "0.5rem",
+      borderRadius: 10,
+      paddingHorizontal: 15,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+    },
+  });
