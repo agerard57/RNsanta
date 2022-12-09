1c7c0afbf75ebbc515f70137049e952f6ce03f4e
WildTrack <wildryx@outlook.fr>
2022-12-09 21:56:12 +0100

    feat: :lipstick: Add scroll to content of longer page

--- client/src/navigation/components/Navigation.tsx
+++ client/src/navigation/components/Navigation.tsx
@@ -9,6 +9,8 @@ import { Layout } from "../../layout";
 import { LoginPage, RegisterPage } from "../../authPage";
 import { RootTabParamList } from "../../types";
 import { LinkingConfiguration } from "./LinkingConfiguration";
+import { View, StyleSheet } from "react-native";
+import { ThemeType } from "../../theme";
 
 export const Navigation = () => {
   const { Navigator, Screen } = createNativeStackNavigator<RootTabParamList>();
@@ -27,39 +29,49 @@ export const Navigation = () => {
       theme={NavContainerTheme}
     >
       <Layout>
-        <Navigator initialRouteName="Home">
-          <Screen
-            name="Home"
-            component={HomePage}
-            options={{ headerShown: false }}
-          />
-          <Screen
-            name="Hoods"
-            component={HoodsPage}
-            options={{ headerShown: false }}
-          />
-          <Screen
-            name="Clock"
-            component={ClockPage}
-            options={{ headerShown: false }}
-          />
-          <Screen
-            name="Login"
-            component={LoginPage}
-            options={{ headerShown: false }}
-          />
-          <Screen
-            name="Register"
-            component={RegisterPage}
-            options={{ headerShown: false }}
-          />
-          <Screen
-            name="HoodDetails"
-            component={HoodDetailsPage}
-            options={{ headerShown: false }}
-          />
-        </Navigator>
+        <View style={styles().view}>
+          <Navigator initialRouteName="Home">
+            <Screen
+              name="Home"
+              component={HomePage}
+              options={{ headerShown: false }}
+            />
+            <Screen
+              name="Hoods"
+              component={HoodsPage}
+              options={{ headerShown: false }}
+            />
+            <Screen
+              name="Clock"
+              component={ClockPage}
+              options={{ headerShown: false }}
+            />
+            <Screen
+              name="Login"
+              component={LoginPage}
+              options={{ headerShown: false }}
+            />
+            <Screen
+              name="Register"
+              component={RegisterPage}
+              options={{ headerShown: false }}
+            />
+            <Screen
+              name="HoodDetails"
+              component={HoodDetailsPage}
+              options={{ headerShown: false }}
+            />
+          </Navigator>
+        </View>
       </Layout>
     </NavigationContainer>
   );
 };
+
+const styles = (theme?: ThemeType) =>
+  StyleSheet.create({
+    view: {
+      overflow: "scroll",
+      flex: 1,
+    }
+  });
