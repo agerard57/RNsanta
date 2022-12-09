c7aa225674af27e9bc8573dd83e922b943bbce54
WildTrack <wildryx@outlook.fr>
2022-12-09 09:44:38 +0100

    feat: :sparkles: Add redirection to login page

--- client/src/core/components/Header.tsx
+++ client/src/core/components/Header.tsx
@@ -1,22 +1,27 @@
 import React, { FC, useContext } from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { ThemeContext, ThemeType } from "../../theme";
+import { useNavigation } from "@react-navigation/native";
 
 export const Header: FC = (props) => {
   const theme = useContext(ThemeContext);
+  const navigation = useNavigation();
 
   return (
     <View style={styles().headerContainer}>
       <Text style={styles(theme).pageTitle}>Santa's Hood</Text>
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Text style={styles(theme).userName}>Padoru</Text>
-        <Image
-          source={{
-            uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/4236d1d9-6a66-4488-ab28-bf744b693173/ddn54uv-2d9156a6-6e13-45f4-a2f5-e0c8fb637418.jpg",
-          }}
-          style={styles().profilePic}
-        />
-      </View>
+      <TouchableWithoutFeedback
+        onPress={() => {navigation.navigate("Login")}}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Text style={styles(theme).userName}>Padoru</Text>
+          <Image
+            source={{
+              uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/4236d1d9-6a66-4488-ab28-bf744b693173/ddn54uv-2d9156a6-6e13-45f4-a2f5-e0c8fb637418.jpg",
+            }}
+            style={styles().profilePic}
+            />
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
