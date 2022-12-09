5360f5281e799c0afff1ccc0b4b9b75a3d37ba7c
GERARD Alexandre <agerard57@protonmail.com>
2022-12-09 17:27:13 +0100

    feat: Remove useless prop

--- client/src/clockPage/components/ClockPage.tsx
+++ client/src/clockPage/components/ClockPage.tsx
@@ -2,7 +2,7 @@ import React, { FC, useContext } from "react";
 import { View, StyleSheet } from "react-native";
 
 import { Container, PageTitle } from "../../core";
-import { ThemeContext, ThemeType } from "../../theme";
+import { ThemeContext } from "../../theme";
 import { useClockPage } from "../hooks";
 import { CustomText } from "./CustomText";
 import { DaysDisplay } from "./DaysDisplay";
@@ -14,12 +14,9 @@ export const ClockPage: FC = () => {
   const { countdown } = useClockPage();
 
   return (
-    <View style={styles().page}>
-      <PageTitle
-        additionalStyling={styles().title}
-        title="Christmas countdown"
-      />
-      <Container additionalStyling={styles().clock}>
+    <View style={styles.page}>
+      <PageTitle additionalStyling={styles.title} title="Christmas countdown" />
+      <Container additionalStyling={styles.clock}>
         <CustomText type="main" theme={theme}>
           Countdown before Santa Claus comes down your chimney!
         </CustomText>
@@ -36,28 +33,27 @@ export const ClockPage: FC = () => {
   );
 };
 
-const styles = (theme?: ThemeType) =>
-  StyleSheet.create({
-    page: {
-      paddingHorizontal: 16,
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "transparent",
-    },
-    title: {
-      alignItems: "center",
-      justifyContent: "center",
-      width: "100%",
-      borderRadius: 39,
-      paddingBottom: 16,
-      padding: 0,
-    },
-    clock: {
-      width: "100%",
-      borderRadius: 39,
-      marginBottom: 56,
-      padding: 30,
-      overflow: "hidden",
-    },
-  });
+const styles = StyleSheet.create({
+  page: {
+    paddingHorizontal: 16,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  title: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    borderRadius: 39,
+    paddingBottom: 16,
+    padding: 0,
+  },
+  clock: {
+    width: "100%",
+    borderRadius: 39,
+    marginBottom: 56,
+    padding: 30,
+    overflow: "hidden",
+  },
+});
